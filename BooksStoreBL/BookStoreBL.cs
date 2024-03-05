bc86a3df2dee379038f7b2d40876fb35ed8edef1
RacheliGo <racheli5910@gmail.com>
2024-03-05 21:50:20 +0200

    Add project files.

---
+++ BooksStoreBL/BookStoreBL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksStoreDAL;
+using Entities;
+
+namespace BooksStoreBL
+{
+    public class BookStoreBL
+    {
+        List<BookDetails> ListOfBooks;
+        FileConnection fileConnection;
+        public BookStoreBL()
+        {
+            fileConnection = new FileConnection();
+            ListOfBooks = fileConnection.ReadAllBooks();
+        }
+
+        public IEnumerable<BookDetails> GetAllBooks()
+        {
+            return ListOfBooks;
+        }
+        public IEnumerable<BookDetails> HighCostBooks()
+        {
+            var highCost = ListOfBooks.Where(l => l.Price > 30).OrderBy(l=>l.Id);
+            return highCost;
+        }
+        public IEnumerable<BookDetails> CostOfComics() 
+        {
+            var costComics = ListOfBooks.Where(l => l.IsComics);
+            return costComics;
+        }
+        public IEnumerable<BookDetails> NameBookForGirl()
+        {
+            var costComics = ListOfBooks.Where(l => (l.MinAge>=5 && l.MinAge<10) || (l.MaxAge>5&&l.MaxAge<=10));
+            return costComics;
+        }
+    }
+}
---
+++ BooksStoreGUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BooksStoreGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BooksStoreBL.BookStoreBL booksStoreBL; 
+        public MainWindow()
+        {
+            booksStoreBL = new BooksStoreBL.BookStoreBL();
+            
+            InitializeComponent();
+            var res = booksStoreBL.GetAllBooks();
+            BooksDataGrid.ItemsSource = res;
+
+            var reselt = booksStoreBL.HighCostBooks();
+            highCost.ItemsSource = reselt;
+
+            var reseltC = booksStoreBL.CostOfComics().Select(l => l.Price);
+            CostComics.ItemsSource = reseltC;
+
+            var reseltB = booksStoreBL.NameBookForGirl().Select(l =>l.Name);
+            BookForGirl.ItemsSource = reseltB;
+        }
+    }
+}
